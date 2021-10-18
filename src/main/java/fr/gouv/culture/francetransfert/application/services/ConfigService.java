e7210ff34c721064874e5f08ca6635b1011bee0c
Julien Aubert <j.aubert@actongroup.com>
2021-10-18 20:06:39 +0200

    mimetype

--- src/main/java/fr/gouv/culture/francetransfert/application/services/ConfigService.java
+++ src/main/java/fr/gouv/culture/francetransfert/application/services/ConfigService.java
@@ -2,10 +2,12 @@ package fr.gouv.culture.francetransfert.application.services;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import fr.gouv.culture.francetransfert.application.resources.model.ConfigRepresentation;
+import fr.gouv.culture.francetransfert.francetransfert_metaload_api.MimeService;
 
 @Service
 public class ConfigService {
@@ -13,8 +15,11 @@ public class ConfigService {
 	@Value("${extension.name}")
 	private List<String> extensionList;
 
-	@Value("${mimetype.list}")
+	@Value("${mimetype.front}")
 	private List<String> mimeList;
+
+	@Autowired
+	MimeService mimeService;
 
 	public ConfigRepresentation getConfig() {
 		return ConfigRepresentation.builder().extension(extensionList).mimeType(mimeList).build();
