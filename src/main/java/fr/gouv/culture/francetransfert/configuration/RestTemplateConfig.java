a5495c02b88c39b0c9d9a07e7a80ef15c905d06d
Ghazlen Wissem SOLTANI <gw.soltani@actongroup.com>
2022-07-01 15:24:13 +0200

    Revert "Add header in java files"
    This reverts commit f54fa4d827b3f2a2b8349afc96b190e9b0346ad9

--- src/main/java/fr/gouv/culture/francetransfert/configuration/RestTemplateConfig.java
+++ src/main/java/fr/gouv/culture/francetransfert/configuration/RestTemplateConfig.java
@@ -1,10 +1,3 @@
-/*
-  * Copyright (c) Ministère de la Culture (2022) 
-  * 
-  * SPDX-License-Identifier: Apache-2.0 
-  * License-Filename: LICENSE.txt 
-  */
-
 package fr.gouv.culture.francetransfert.configuration;
 
 import java.security.KeyManagementException;
