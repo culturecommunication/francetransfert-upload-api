05bd6c6a1d8b6c25a33a953219557d44c385f370
Julien Aubert <j.aubert@actongroup.com>
2022-08-23 11:20:49 +0200

    get Status

--- src/main/java/fr/gouv/culture/francetransfert/application/resources/upload/PublicResources.java
+++ src/main/java/fr/gouv/culture/francetransfert/application/resources/upload/PublicResources.java
@@ -14,11 +14,6 @@
 
 package fr.gouv.culture.francetransfert.application.resources.upload;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.UUID;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
@@ -34,23 +29,16 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-
 import fr.gouv.culture.francetransfert.application.resources.model.FileRepresentation;
-import fr.gouv.culture.francetransfert.application.resources.model.FranceTransfertDataRepresentation;
 import fr.gouv.culture.francetransfert.application.resources.model.InitialisationInfo;
 import fr.gouv.culture.francetransfert.application.resources.model.PackageInfoRepresentation;
-import fr.gouv.culture.francetransfert.application.resources.model.RecipientInfo;
 import fr.gouv.culture.francetransfert.application.resources.model.StatusInfo;
-import fr.gouv.culture.francetransfert.application.resources.model.StatusRepresentation;
 import fr.gouv.culture.francetransfert.application.resources.model.ValidateData;
 import fr.gouv.culture.francetransfert.application.resources.model.ValidateUpload;
 import fr.gouv.culture.francetransfert.application.services.ValidationMailService;
-import fr.gouv.culture.francetransfert.core.enums.EnclosureKeysEnum;
 import fr.gouv.culture.francetransfert.core.exception.MetaloadException;
 import fr.gouv.culture.francetransfert.core.exception.StatException;
 import fr.gouv.culture.francetransfert.core.exception.StorageException;
-import fr.gouv.culture.francetransfert.core.utils.RedisUtils;
 import fr.gouv.culture.francetransfert.domain.exceptions.ApiValidationException;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -80,36 +68,33 @@ public class PublicResources {
 		}
 		return validationMailService.validateMailData(metadata, headerAddr, remoteAddr);
 	}
-	
-	//---
+
+	// ---
 	@PostMapping("/uploadPli")
 	@Operation(method = "POST", description = "uploadPli")
 	public InitialisationInfo uploadData(HttpServletResponse response, HttpServletRequest request,
-			 @RequestParam("numMorceauFichier") int flowChunkNumber,
-				@RequestParam("totalMorceauxFichier") int flowTotalChunks, @RequestParam("tailleMorceauFichier") long flowChunkSize,
-				@RequestParam("tailleFichier") long flowTotalSize, @RequestParam("idFichier") String flowIdentifier,
-				@RequestParam("nomFichier") String flowFilename, @RequestParam("fichier") MultipartFile file,
-				@RequestParam("idPli") String enclosureId,
-				@RequestParam("courrielExpediteur") String senderId 
-			) throws ApiValidationException, MetaloadException, StorageException{
-		
-		 ValidateUpload metadata = new ValidateUpload();
-		 FileRepresentation rootFile = new FileRepresentation();
-		 
-		 rootFile.setFid(flowIdentifier);
-		 rootFile.setSize(flowTotalSize);
-		 rootFile.setName(flowFilename);
-		
-		 
-		 metadata.setEnclosureId(enclosureId);
-		 metadata.setFlowChunkNumber(flowChunkNumber);
-		 metadata.setSenderEmail(senderId);
-		 metadata.setFlowChunkSize(flowChunkSize);
-		 metadata.setFlowTotalChunks(flowTotalChunks);
-		 metadata.setFichier(file);
-		 metadata.setRootFiles(rootFile);
+			@RequestParam("numMorceauFichier") int flowChunkNumber,
+			@RequestParam("totalMorceauxFichier") int flowTotalChunks,
+			@RequestParam("tailleMorceauFichier") long flowChunkSize, @RequestParam("tailleFichier") long flowTotalSize,
+			@RequestParam("idFichier") String flowIdentifier, @RequestParam("nomFichier") String flowFilename,
+			@RequestParam("fichier") MultipartFile file, @RequestParam("idPli") String enclosureId,
+			@RequestParam("courrielExpediteur") String senderId)
+			throws ApiValidationException, MetaloadException, StorageException {
 
-	
+		ValidateUpload metadata = new ValidateUpload();
+		FileRepresentation rootFile = new FileRepresentation();
+
+		rootFile.setFid(flowIdentifier);
+		rootFile.setSize(flowTotalSize);
+		rootFile.setName(flowFilename);
+
+		metadata.setEnclosureId(enclosureId);
+		metadata.setFlowChunkNumber(flowChunkNumber);
+		metadata.setSenderEmail(senderId);
+		metadata.setFlowChunkSize(flowChunkSize);
+		metadata.setFlowTotalChunks(flowTotalChunks);
+		metadata.setFichier(file);
+		metadata.setRootFiles(rootFile);
 
 		String headerAddr = request.getHeader(KEY);
 		String remoteAddr = request.getHeader(FOR);
@@ -118,17 +103,19 @@ public class PublicResources {
 		}
 		return validationMailService.validateUpload(metadata, headerAddr, remoteAddr, senderId, flowIdentifier);
 	}
-	
-	@PostMapping("/statutPli")
-	@Operation(method = "POST", description = "statutPli")
+
+	@GetMapping("/statutPli")
+	@Operation(method = "GET", description = "statutPli")
 	public InitialisationInfo packageStatus(HttpServletResponse response, HttpServletRequest request,
-			@Valid @RequestBody StatusInfo metadata) throws ApiValidationException, MetaloadException {
+			@RequestParam("idPli") String enclosureId, @RequestParam("courrielExpediteur") String senderMail)
+			throws ApiValidationException, MetaloadException {
 
 		String headerAddr = request.getHeader(KEY);
 		String remoteAddr = request.getHeader(FOR);
 		if (remoteAddr == null || "".equals(remoteAddr)) {
 			remoteAddr = request.getRemoteAddr();
 		}
+		StatusInfo metadata = new StatusInfo(enclosureId, senderMail);
 		return validationMailService.getStatusPli(metadata, headerAddr, remoteAddr);
 
 	}
@@ -136,16 +123,17 @@ public class PublicResources {
 	@GetMapping("/donneesPli")
 	@Operation(method = "GET", description = "donneesPli")
 	public PackageInfoRepresentation packageInfo(HttpServletResponse response, HttpServletRequest request,
-			@RequestParam("idPli") String idPli, @RequestParam("courrielExpediteur") String courrielExpediteur) throws ApiValidationException, MetaloadException, StatException {
+			@RequestParam("idPli") String idPli, @RequestParam("courrielExpediteur") String courrielExpediteur)
+			throws ApiValidationException, MetaloadException, StatException {
 
 		String headerAddr = request.getHeader(KEY);
 		String remoteAddr = request.getHeader(FOR);
 		if (remoteAddr == null || "".equals(remoteAddr)) {
 			remoteAddr = request.getRemoteAddr();
 		}
-		
+
 		StatusInfo metadata = new StatusInfo();
-		metadata.setEnclosureId(idPli); 
+		metadata.setEnclosureId(idPli);
 		metadata.setSenderMail(courrielExpediteur);
 		return validationMailService.getInfoPli(metadata, headerAddr, remoteAddr);
 
