71a5d893acd25aa74f57568fd0c0c7ffc40d99df
Jenkins <jenkins@ip-10-8-17-125.eu-west-2.compute.internal>
2023-05-24 12:01:42 +0200

    Publishing 3.12.3

---
+++ src/main/java/fr/gouv/culture/francetransfert/application/resources/model/HealthCheckRepresentation.java
@@ -0,0 +1,33 @@
+/*
+  * Copyright (c) Ministère de la Culture (2022) 
+  * 
+  * SPDX-License-Identifier: Apache-2.0 
+  * License-Filename: LICENSE.txt 
+  */
+
+package fr.gouv.culture.francetransfert.application.resources.model;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class HealthCheckRepresentation {
+
+	private boolean redis;
+	private boolean s3;
+	private boolean smtp;
+	private boolean config;
+	private String smtpUuid;
+	private int smtpDelay;
+	private int smtpPending;
+	private boolean smtpDelayOk;
+
+	public boolean isFtError() {
+		if (redis == false || s3 == false || smtp == false || config == false || smtpDelayOk == false) {
+			return true;
+		}
+		return false;
+	}
+
+}
